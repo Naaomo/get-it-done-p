5d3525344459f13dc927dbe5dcb6bb75ff0b481b
JM066 <mina.lee066@gmail.com>
2020-07-15 01:32:57 +0200

    login and sign up buttons completed

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './App.css';
 import GiveService from './components/GiveService';
 import GetService from './components/GetService';
+
 // import Home from './components/Home';
 
 
@@ -14,9 +15,9 @@ class App extends React.Component {
     this.state = {
       showSignup: false,
       showLogin: false,
-      setShow: false,
       email: "",
       password: "",
+      confirmPassword: '',
       errorMessage: ""
     };
 
@@ -36,9 +37,13 @@ class App extends React.Component {
   };
   handleChange = event => {
     const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
+    const confirmPassword = event.target.value;
+    const password = event.target.value;
+    if (confirmPassword === password) {
+      return this.setState({
+        [name]: value
+      });
+    }
   };
 
   handleLogin = () => {
@@ -51,6 +56,8 @@ class App extends React.Component {
     //     "Content-Type": "application/json"
     //   },
     //   body: JSON.stringify({
+          // fname: this.state.firstName,
+          // lname: this.state.lastName,
     //     email: this.state.email,
     //     password: this.state.password
     //   })
@@ -69,9 +76,8 @@ class App extends React.Component {
   render() {
     return (
       <>
-      <div className="login_nav_container">
+      <div className="login_container">
           <Router>
-            <div> 
             <ul className="links">
               {/* <li>
                 <Link to="/">Home </Link>
@@ -94,42 +100,52 @@ class App extends React.Component {
                   <GetService />
                 </Route>
               </Switch>
-            </div>
-          </Router>
-          <Button variant="light" onClick={() => this.handleModelSign()}>Sign Up</Button>
-          <Button variant="light" onClick={() => this.handleModelLogin()}>Login</Button>
 
-          <Modal show={this.state.showSignup} onHide={this.state.showLogin}>
-            <Modal.Header>
-              Sign up now!
-            </Modal.Header>
-            <Modal.Body>
-                <form>
-                  <input type="text" name="email" onChange={this.handleChange} />
-                  <input type="password" name="password" onChange={this.handleChange}/>
-                  {/* <button onClick={this.handleLogin}>Sign up</button> */}
-                </form>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={() => {this.handleSubmit()}}>Submit</Button>
-            </Modal.Footer>
-         </Modal>
-          <Modal show={this.state.showLogin} onHide={ this.state.showSignup}>
-            <Modal.Header>
-              Login
-            </Modal.Header>
-            <Modal.Body>
-                <form>
-                  <input type="text" name="email" onChange={this.handleChange} />
-                  <input type="password" name="password" onChange={this.handleChange}/>
-                  {/* <button onClick={this.handleSubmit}>Log in</button> */}
-                </form>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={() => {this.handleLogin()}}>Login</Button>
-            </Modal.Footer>
-          </Modal>
+          </Router>
+          <button className="btn btn-light" onClick={() => this.handleModelSign()}>Sign Up</button>
+          <button className="btn btn-light" onClick={() => this.handleModelLogin()}>Login</button>
         </div>
+          <Modal show={this.state.showSignup} 
+                 onHide={this.state.showLogin}>
+                <div className="form-sign-up">
+                  <div className="sign-up">
+                  <h2>Sign Up</h2>
+		                <p>Please fill in this form to create an account!</p>
+                      <hr/>
+                      <input type="text" className="name" placeholder="First Name" name="firstName" onChange={this.handleChange}/>  
+                      <input type="text" className="name" placeholder="Last Name" name="lastName" onChange={this.handleChange}/>  
+                      <input type="text" className="email" placeholder="email"name="email" onChange={this.handleChange}/>           
+                      <input type="password" className="password" placeholder="Password" name="password" onChange={this.handleChange}/>
+                      <input type="password" className="password" placeholder="Confirm Password" name="confirmPassword" onChange={this.handleChange}/>
+                  </div>
+                  <div className="button-container">
+                    <div className="btns">
+                      <button className="btn btn-primary"><i class="fab fa-facebook-f"></i>Sign up with <span>Facebook</span></button>
+                      <button className="btn btn-danger"><i class="fa fa-google"></i>Sign up with <span>Google</span></button>
+                    </div>  
+                    <button className="login-signup-button" onClick={() => {this.handleSubmit()}}>Signup</button>
+                  </div>
+                </div>         
+         </Modal> 
+
+         <Modal show={this.state.showLogin} onHide={ this.state.showSignup}>
+         <div className="form">
+                  <div className="sign-up">
+                      <span class="fa fa-user"></span>
+                      <input type="text" className="email" placeholder="email"name="email" onChange={this.handleChange}/>          
+                      <br></br>
+                      <span class="fa fa-lock"></span>
+                      <input type="password" className="password" placeholder="Password" name="password" onChange={this.handleChange}/>
+                  </div>
+                  <div className="button-container">
+                    <div className="btns">
+                      <button className="btn btn-primary"><i class="fab fa-facebook-f"></i> Login with <span>Facebook</span></button>
+                      <button className="btn btn-danger"><i class="fa fa-google"></i> Login with <span>Google</span></button>
+                    </div>  
+                    <button className="login-signup-button" onClick={() => {this.handleLogin()}}>Login</button>
+                  </div>
+                </div>   
+          </Modal>
       </>
     );
   }

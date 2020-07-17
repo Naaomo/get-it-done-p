e0724ac282517fcc48754ed3659b7a486fb1439d
JM066 <mina.lee066@gmail.com>
2020-07-17 17:42:19 +0200

    nearby location button and country dropdown is added

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './search.css';
-import { Button, Modal } from 'react-bootstrap';
-import Geonames from 'geonames.js'; 
+// import { Button, Modal } from 'react-bootstrap';
+// import Geonames from 'geonames.js'; 
+import 'opencage-api-client';
+const OCD_API_KEY = process.env.REACT_APP_OCD_API_KEY;
+
 
 // const geonames = new Geonames({
 //   username: 'mina066',
@@ -16,6 +19,7 @@ class Search extends React.Component {
     this.state = {
       showLocation: false,
       location: [],
+      cityNearby: [],
       data: [],
       selectedData: [],
       name: []
@@ -71,9 +75,33 @@ class Search extends React.Component {
     this.setState({
       data: dataObject
     })
-    console.log("something");
-    this.showDropdown()
+    this.showDropdown();
+
+
   }
+
+  getLocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.showPosition);
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    } 
+    }
+
+  showPosition = (position) => {
+    const { latitude, longitude } = position.coords;
+    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${OCD_API_KEY}`)
+    .then(response =>  response.json()) 
+    .then(data => {
+      console.log(data.results[0].components.city);
+      const { cityNearby } = this.state;
+      cityNearby.push(data.results[0].components.city)
+      this.setState({
+        cityNearby: cityNearby
+      })
+    })  
+    }
+
 
   async handleClick() {
     const selected = this.state.selected;
@@ -98,22 +126,49 @@ class Search extends React.Component {
   // }
 
   async showDropdown() {
-
+      const location = await fetch('https://api.first.org/data/v1/countries');
+      const result = await location.json();
+      Object.keys(result.data).forEach((key, index) => {
+        const { location } = this.state;
+        location.push(result.data[key].country)
+        this.setState({
+          location: location
+      })
+    })
+      // let countries = [];
+      // let countryData = Object.values(result.data);
+      // for (let country of countryData){
+      // countries.push(country.country);
+      // }
+      // console.log(countries);
   }
   
   render() {
-    const country = this.state.country;
+   
         return (
           <>
-            <input type="text" name="country" onChange={this.handleChange}/>
-            {/* <select type="select" onChange={this.onSelected}>
+            <div className="checkbox-container">         
+              <input type="checkbox" onChange={this.handleChange} className="checkbox" id="1" name="cleaning" value="clearning"/>
+                <label for="clearning">Cleaning</label>
+              <input type="checkbox" onChange={this.handleChange} className="checkbox" id="2" name="gardening" value="gardening"/>
+                <label for="clearning">Gardening</label>
+              <input type="checkbox" onChange={this.handleChange} className="checkbox" id="3" name="moving" value="moving"/>
+                <label for="clearning">Moving</label>
+              <input type="checkbox" onChange={this.handleChange} className="checkbox" id="4" name="plumbing" value="plumbing"/>
+                <label for="clearning">Plumbing</label>
+                <br></br>
+            </div> 
+            <div>
+              <button onClick={() => this.getLocation()}>Find service nearby</button>
+              <select type="select">
+                <option selected hidden>Select your country</option>
               {this.state.location.map((city, index) => { 
                return <option key={index} value={index}>{city}</option>
               })
-             }
-             </select> */}
+              } 
+            </select>
+            </div>
             
-            <input type="text" name="serviceType" onChange={this.handleChange}/>
             <button onClick={() => this.handleClick()}>Search</button>
             {/* <Modal show={this.state.showLocationPopup} 
                  onHide={!this.state.showLocationPopup}> */}

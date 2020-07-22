d16a2f488c135bf34a6a93459e4bb6b72435c270
Nanette Taylor <nanette.m.taylor@gmail.com>
2020-07-22 03:43:16 +0000

    Revert "Kind of done with MapView"

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import GiveService from './components/GiveService';
 import GetService from './components/GetService';
 import Login from './components/Login';
 import Main from './components/Main';
-import MapView from "./components/MapView";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Redirect, Route, Link, NavLink } from "react-router-dom";
 
@@ -31,37 +30,33 @@ class App extends React.Component {
       
       // const {referrer} = this.state;
       return (
-          <div className="app">
-              <Router>
-                  <nav className="py-3 navbar navbar-expand-lg sticky-top navbar-light" style={{top: 0}}>
-                      <div className="container">
-                          <Link className="navbar-brand py-0" to="/"><span></span><img src="./images/logo.png"/></Link>
-                          <button className="navbar-toggler" type="button" data-toggle="collapse"
-                                  data-target="#navbar-content" aria-controls="navbar-content" aria-expanded="false"
-                                  aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span>
-                          </button>
-                          <div className="collapse navbar-collapse" id="navbar-content">
-                              <ul className="navbar-nav ml-auto">
-                                <li className="nav-item mx-1"><Link to="/services">Do-ers</Link></li>
-                                  <Login />
-                              </ul>
+                  <Router>
+                      <nav className="py-3 navbar navbar-expand-lg sticky-top navbar-light" style={{top: 0}}>
+                          <div className="container">
+                              <Link className="navbar-brand py-0" to="/"><span></span><img src="./images/logo.png"/></Link>
+                              <button className="navbar-toggler" type="button" data-toggle="collapse"
+                                      data-target="#navbar-content" aria-controls="navbar-content" aria-expanded="false"
+                                      aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span>
+                              </button>
+                              <div className="collapse navbar-collapse" id="navbar-content">
+                                  <ul className="navbar-nav ml-auto">
+                                    <li className="nav-item mx-1"><Link to="/services">Do-ers</Link></li>
+                                      <Login />
+                                  </ul>
+                              </div>
                           </div>
-                      </div>
-                  </nav>
-                  <Route exact path="/">
-                    <Main getProviders={(providersData) => this.getProviders(providersData)}/>
-                  </Route>
-                  <Route exact path="/getService">
-                    <GetService providersList = {this.state.providers} />
-                  </Route>
-                  <Route exact path="/services">
-                      <GiveService/>
-                  </Route>
-                  <Route exact path="/map">
-                      <MapView />
-                  </Route>
-            </Router>
-          </div>
+                      </nav>
+                      <Route exact path="/">
+                        <Main getProviders={(providersData) => this.getProviders(providersData)}/>
+                      </Route>
+                      <Route exact path="/getService">
+                        <GetService providersList = {this.state.providers} />
+                      </Route>
+                      <Route exact path="/services">
+                          <GiveService/>
+                      </Route>
+                </Router>
+              
       );
   }
 }
